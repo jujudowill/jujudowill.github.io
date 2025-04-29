0630702ddb0358fdd4b123bec8ef28ad93aa7581
= <=>
2025-04-28 21:43:18 -0300

    alteração 1

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ document.querySelector("#yes").addEventListener("click", function () {
   let question = document.getElementById("question");
   let message = document.getElementById("answer");
 
-  card.style.backgroundImage =
-    "url('https://img1.picmix.com/output/stamp/normal/5/1/0/1/1171015_f458c.gif')";
+  
   question.style.display = "none";
   message.style.display = "flex";
 });
